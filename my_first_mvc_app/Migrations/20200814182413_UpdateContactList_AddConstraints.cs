f90dd239c98ac7a56a21b69aefda078313bbb19b
Scheirer Nicolae (Miki) <58042678+mscheirer@users.noreply.github.com>
2020-08-14 21:32:56 +0300

    Comanda SQL pt stergerea din Baza de Date

---
+++ my_first_mvc_app/Migrations/20200814182413_UpdateContactList_AddConstraints.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace my_first_mvc_app.Migrations
+{
+    public partial class UpdateContactList_AddConstraints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                    DELETE FROM 
+                        ContactListEntry
+                    WHERE
+                        Name IS NULL
+                        OR LEN(Name) > 100
+                        OR LEN(PhoneNumber) > 100
+                        OR LEN(Email) > 100");
+
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "ContactListEntry",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ContactListEntry",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "ContactListEntry",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "ContactListEntry",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ContactListEntry",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "ContactListEntry",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
--- my_first_mvc_app/Models/ContactListEntry.cs
+++ my_first_mvc_app/Models/ContactListEntry.cs
@@ -2,21 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace my_first_mvc_app.Models
 {
     public class ContactListEntry
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
 
+        [Required]
         public ContactType ContactType { get; set; }
 
+        [Display(Name = "Contact Type")]
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
 
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
         public DateTime? DateOfBirth { get; set; }
 
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(50)]
         public string PhoneNumber { get; set; }
 
+        [Display(Name = "Email")]
+        [StringLength(50)]
         public string Email { get; set; }
     }
 
