aae44a95d4ac57f9c8173b9cb719de868f3c57bb
Scheirer Nicolae (Miki) <58042678+mscheirer@users.noreply.github.com>
2020-08-14 11:19:48 +0300

    ContactListEntry

---
+++ my_first_mvc_app/Models/ContactListEntry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_first_mvc_app.Models
+{
+    public class ContactListEntry
+    {
+        public int Id { get; set; }
+
+        public ContactType ContactType { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+    }
+
+    public enum ContactType
+    {
+        Person = 0,
+
+        Company
+    }
+}
